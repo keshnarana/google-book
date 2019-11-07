b4dcebf5f0c4b8eac92b2db2ff2c781f003711b6
kesh24 <keshnarana4@gmail.com>
2019-11-07 13:53:31 -0500

    start

--- client/src/components/Logo/index.js
+++ client/src/components/Logo/index.js
@@ -4,7 +4,7 @@ import "./style.css"
 function Logo() {
     return (
             <div className="logo">
-                <img className="logo-position" src="http://icons.iconarchive.com/icons/dtafalonso/android-lollipop/256/Play-Books-icon.png" />
+           
                 <h1 className="logo-text logo-position"> (React) Google Play Books Search </h1>
             </div>
     )
