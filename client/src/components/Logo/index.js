7ac42b2ec2db7bc59032d5b4a2ecbf11d9e653c3
kesh24 <keshnarana4@gmail.com>
2019-11-07 14:14:14 -0500

    start

--- client/src/components/Logo/index.js
+++ client/src/components/Logo/index.js
@@ -4,7 +4,7 @@ import "./style.css"
 function Logo() {
     return (
             <div className="logo">
-           
+              
                 <h1 className="logo-text logo-position"> (React) Google Play Books Search </h1>
             </div>
     )
